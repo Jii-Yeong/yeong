c7e940241d005c022d44fac5d9c8d736cdb4bbf5
Jii-Yeong <tj5134@naver.com>
2024-11-04 14:55:03 +0900

    feat: [@yeong/utils] date utils 문자열을 js date 객체로 바꿔주는 utils 함수 작성

--- packages/utils/src/date/date.utils.ts
+++ packages/utils/src/date/date.utils.ts
@@ -1,5 +1,9 @@
 import { DateTime } from 'luxon';
 
+export const transferStringToDate = (value: string, format = 'yyyy-MM-dd') => {
+  return DateTime.fromFormat(value, format).toJSDate();
+};
+
 export const formatDateToString = (date: Date) => {
   return DateTime.fromJSDate(date).toFormat('yyyy.MM.dd');
 };
