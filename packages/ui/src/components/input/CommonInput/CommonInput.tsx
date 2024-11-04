40042a0a48d52f7c61f252456df615d190b44a6d
Jii-Yeong <tj5134@naver.com>
2024-11-04 14:55:50 +0900

    fix: [@yeong/ui] CommonInput 컴포넌트에 경고 문구 추가

--- packages/ui/src/components/input/CommonInput/CommonInput.tsx
+++ packages/ui/src/components/input/CommonInput/CommonInput.tsx
@@ -1,18 +1,20 @@
-import { parseDomSizeValue } from '@yeong/utils/string'
-import { ChangeEvent, CSSProperties, useState, KeyboardEvent } from 'react'
+import { parseDomSizeValue } from '@yeong/utils/string';
+import { ChangeEvent, CSSProperties, KeyboardEvent, useState } from 'react';
+import { UI_COLORS } from '../../../constants/color.constants.ts';
 
 export type CommonInputProps = {
-  defaultValue?: string
-  width?: string | number
-  height?: string | number
-  borderRadius?: string | number
-  padding?: string | number
-  placeholder?: string
-  style?: CSSProperties
-  type?: string
-  setInputValue: (value: string) => void
-  pressEnter?: () => void
-}
+  defaultValue?: string;
+  width?: string | number;
+  height?: string | number;
+  borderRadius?: string | number;
+  padding?: string | number;
+  placeholder?: string;
+  style?: CSSProperties;
+  type?: string;
+  alertText?: string;
+  setInputValue: (value: string) => void;
+  pressEnter?: () => void;
+};
 
 export default function CommonInput({
   defaultValue = '',
@@ -23,33 +25,42 @@ export default function CommonInput({
   placeholder,
   style,
   type = 'text',
+  alertText,
   setInputValue,
   pressEnter,
 }: CommonInputProps) {
-  const [value, setValue] = useState(defaultValue)
+  const [value, setValue] = useState(defaultValue);
   const changeInputValue = (e: ChangeEvent) => {
-    const element = e.target as HTMLInputElement
-    setValue(element.value)
-    setInputValue(element.value)
-  }
+    const element = e.target as HTMLInputElement;
+    setValue(element.value);
+    setInputValue(element.value);
+  };
   const handleKeyUp = (e: KeyboardEvent) => {
-    if (pressEnter && e.code === 'Enter') pressEnter()
-  }
+    if (pressEnter && e.code === 'Enter') pressEnter();
+  };
   return (
-    <input
-      className="border border-gray focus:outline-main text-black"
+    <div
+      className="flex flex-col relative"
       style={{
         width: parseDomSizeValue(width),
         height: parseDomSizeValue(height),
-        borderRadius: parseDomSizeValue(borderRadius),
-        padding: parseDomSizeValue(padding),
         ...style,
       }}
-      placeholder={placeholder}
-      value={value}
-      onChange={changeInputValue}
-      onKeyUp={handleKeyUp}
-      type={type}
-    />
-  )
+    >
+      <input
+        className="border border-gray focus:outline-main text-black w-full h-full"
+        style={{
+          borderRadius: parseDomSizeValue(borderRadius),
+          padding: parseDomSizeValue(padding),
+          borderColor: alertText ? UI_COLORS.red : UI_COLORS.gray,
+        }}
+        placeholder={placeholder}
+        value={value}
+        onChange={changeInputValue}
+        onKeyUp={handleKeyUp}
+        type={type}
+      />
+      <p className="text-md text-red absolute bottom-[-20px]">{alertText}</p>
+    </div>
+  );
 }
