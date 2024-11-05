c6aec8f9f25bab7da841e988dc45225d297710dc
Jii-Yeong <tj5134@naver.com>
2024-11-05 10:45:48 +0900

    lint: [readly-api] 수정된 lint 적용

--- apps/readly-api/utils/auth.ts
+++ apps/readly-api/utils/auth.ts
@@ -1,7 +1,9 @@
-import { GoogleTokenType, GoogleUserInfoType } from "../model/auth";
 import jwt from 'jsonwebtoken';
+import { GoogleTokenType, GoogleUserInfoType } from '../model/auth';
 
-export const getGoogleTokenByCode = async (code: string): Promise<GoogleTokenType | null> => {
+export const getGoogleTokenByCode = async (
+  code: string,
+): Promise<GoogleTokenType | null> => {
   try {
     const response = await fetch('https://oauth2.googleapis.com/token', {
       method: 'POST',
@@ -15,48 +17,51 @@ export const getGoogleTokenByCode = async (code: string): Promise<GoogleTokenTyp
         redirect_uri: process.env.GOOGLE_REDIRECT_URI || '',
         grant_type: 'authorization_code',
       }),
-    })
+    });
 
-    const data = await response.json()
-    return data
+    const data = await response.json();
+    return data;
   } catch (error) {
     console.error('Error exchanging authorization code:', error);
-    return null
+    return null;
   }
-}
+};
 
-export const getGoogleUserInfo = async (accessToken: string): Promise<GoogleUserInfoType> => {
+export const getGoogleUserInfo = async (
+  accessToken: string,
+): Promise<GoogleUserInfoType> => {
   const userInfo = await fetch(
     'https://www.googleapis.com/oauth2/v1/userinfo?alt=json',
     {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
-    }
-  )
+    },
+  );
 
-  return userInfo.json()
-}
+  return userInfo.json();
+};
 
 export const generateJwtToken = (payload: Pick<GoogleUserInfoType, 'id'>) => {
-  const secretKey = process.env.JWT_SECRET_KEY
-  const token = jwt.sign(payload, secretKey, { expiresIn: '1h' })
+  const secretKey = process.env.JWT_SECRET_KEY;
+  const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
+  return token;
+};
 
-  return token
-}
-
-export const decodeJwtToken = (accessToken: string): Pick<GoogleUserInfoType, 'id'> => {
-  const secretKey = process.env.JWT_SECRET_KEY
+export const decodeJwtToken = (
+  accessToken: string | null,
+): Pick<GoogleUserInfoType, 'id'> => {
+  const secretKey = process.env.JWT_SECRET_KEY;
 
   try {
-    const isVerify = jwt.verify(accessToken, secretKey)
+    const isVerify = jwt.verify(accessToken, secretKey);
 
-    if (!isVerify) throw new Error('invalid token')
+    if (!isVerify) throw new Error('invalid token');
 
-    const decoded = jwt.decode(accessToken)
+    const decoded = jwt.decode(accessToken);
 
-    return decoded
+    return decoded;
   } catch (e) {
-    return e
+    return e;
   }
-}
+};
