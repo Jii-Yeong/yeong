03e6ab9d2c73c8413107b8e9bf5d51d9d6c76725
Jii-Yeong <tj5134@naver.com>
2024-11-06 16:10:27 +0900

    chore: SummaryCommentInput story 타입에러 수정

--- apps/readly/src/components/comment/SummaryCommentInput/SummaryCommentInput.stories.ts
+++ apps/readly/src/components/comment/SummaryCommentInput/SummaryCommentInput.stories.ts
@@ -10,5 +10,8 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    clickInputButton: async () => {},
+    setCommentValue: () => {},
+  },
 };
