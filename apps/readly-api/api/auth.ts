143a5f57361d37ac23a8eff7b7ae6f7519c79b83
Jii-Yeong <tj5134@naver.com>
2024-11-06 18:17:26 +0900

    log: [readly-api] auth/login api userInfo 로그 작성

--- apps/readly-api/api/auth.ts
+++ apps/readly-api/api/auth.ts
@@ -1,40 +1,46 @@
-import express, { Request, Response } from 'express';
-import { generateJwtToken, getGoogleTokenByCode, getGoogleUserInfo } from '../utils/auth';
 import { sql } from '@vercel/postgres';
+import express, { Request, Response } from 'express';
+import {
+  generateJwtToken,
+  getGoogleTokenByCode,
+  getGoogleUserInfo,
+} from '../utils/auth';
 import { isExistRows } from '../utils/sql';
 
-const authRouter = express.Router()
+const authRouter = express.Router();
 
 authRouter.post('/login', async (req: Request, res: Response) => {
-  const code = req.body.code
+  const code = req.body.code;
 
-  const token = await getGoogleTokenByCode(code)
+  const token = await getGoogleTokenByCode(code);
 
   if (!token) {
-    res.send('login failed')
-    return
+    res.send('login failed');
+    return;
   }
 
-  const userInfo = await getGoogleUserInfo(token.access_token)
+  const userInfo = await getGoogleUserInfo(token.access_token);
 
-  const accessToken = generateJwtToken({ id: userInfo.id })
+  console.log('userInfo', userInfo);
+
+  const accessToken = generateJwtToken({ id: userInfo.id });
 
   const { rows } = await sql`
   SELECT EXISTS (
     SELECT 1 
     FROM users 
     WHERE id = ${userInfo.id}
-  );`
+  );`;
 
   if (isExistRows(rows)) {
-    res.send({ message: 'login success', accessToken })
+    res.send({ message: 'login success', accessToken });
   }
 
   await sql`
   INSERT INTO users 
     (id, email, profile_image, nickname) 
-  VALUES (${userInfo.id}, ${userInfo.email}, ${userInfo.picture}, ${userInfo.name});`
-  res.send({ message: 'regist user', accessToken })
+  VALUES (${userInfo.id}, ${userInfo.email}, ${userInfo.picture}, ${userInfo.name});`;
+  res.send({ message: 'regist user', accessToken });
 });
 
-export default authRouter
+export default authRouter;
