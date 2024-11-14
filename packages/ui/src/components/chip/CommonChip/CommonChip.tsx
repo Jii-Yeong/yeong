a0295e779469649a5f60cd8602b198de347fc8b7
Jii-Yeong <tj5134@naver.com>
2024-11-14 11:49:53 +0900

    fix: [@yeong/ui] 칩 컴포넌트 disabled 상태 추가

--- packages/ui/src/components/chip/CommonChip/CommonChip.tsx
+++ packages/ui/src/components/chip/CommonChip/CommonChip.tsx
@@ -3,29 +3,47 @@ import { UI_COLORS } from '../../../constants/color.constants.ts';
 
 type CommonChipProps = {
   text: string;
+  value?: string;
   className?: string;
   classList?: string;
   backgroundColor?: string;
   activeColor?: string;
   isActive?: boolean;
+  disabled?: boolean;
+  onClick?: (value: string) => void;
 };
 
 export default function CommonChip({
   text,
+  value,
   className,
   classList,
   backgroundColor = UI_COLORS.lightBlue,
   activeColor = UI_COLORS.blue,
   isActive,
+  disabled,
+  onClick,
 }: CommonChipProps) {
   const divClassName = twMerge(
-    'text-[14px] py-[2px] px-[8px] rounded-full cursor-pointer',
-    [
-      isActive ? `bg-[${activeColor}]` : `bg-[${backgroundColor}]`,
-      isActive ? 'text-white' : `text-[${activeColor}]`,
-    ],
+    'text-[14px] py-[2px] px-[8px] rounded-full cursor-pointer w-max hover:opacity-60',
+    [disabled && 'opacity-40  hover:opacity-40', disabled && 'cursor-default'],
     className,
     classList,
   );
-  return <div className={divClassName}>{text}</div>;
+
+  const handleClickChip = () => {
+    if (!disabled && onClick) onClick(value || '');
+  };
+  return (
+    <div
+      className={divClassName}
+      style={{
+        backgroundColor: isActive ? activeColor : backgroundColor,
+        color: isActive ? UI_COLORS.white : activeColor,
+      }}
+      onClick={handleClickChip}
+    >
+      {text}
+    </div>
+  );
 }
