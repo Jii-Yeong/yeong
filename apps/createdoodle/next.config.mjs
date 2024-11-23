33444eb399da8ef11ed0d1f42be8f96fa5af8b4d
Jii-Yeong <tj5134@naver.com>
2024-11-23 20:45:07 +0900

    fix: [createdoodle] 고정되어있는 post api url 값 제거 및 env 값으로 교체

--- apps/createdoodle/next.config.mjs
+++ apps/createdoodle/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_CLIENT_URL: process.env.NEXT_PUBLIC_CLIENT_URL,
+  },
   sassOptions: {
     prependData: "@import '@/styles/colors.scss';",
   },
--- apps/createdoodle/src/api/post.api.ts
+++ apps/createdoodle/src/api/post.api.ts
@@ -1,4 +1,4 @@
 export const getPostMd = async (id: string) => {
-  const response = await fetch(`http://localhost:3001/posts/${id}.md`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_CLIENT_URL}/posts/${id}.md`);
   return response.text();
 };
