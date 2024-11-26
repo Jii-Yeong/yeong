bd61410cb6d975a87a64e3172723629fb829de08
Jii-Yeong <tj5134@naver.com>
2024-11-26 13:56:36 +0900

    fix: [createdoodle] 포스트 리스트 아이템에 gap 추가

--- apps/createdoodle/src/components/item/PostListItem.tsx
+++ apps/createdoodle/src/components/item/PostListItem.tsx
@@ -13,7 +13,7 @@ export default function PostListItem({item}: PostListItemProps) {
   return (
     <Link
       href={`/post/${item.id}`}
-      className="flex flex-row bg-[#ffffff] rounded-lg p-6 min-h-[129px]">
+      className="flex flex-row bg-[#ffffff] rounded-lg p-6 min-h-[129px] gap-x-[32px]">
       <div className="flex flex-col gap-y-2 flex-1">
         <p className="text-[#7b71be]">{item.category}</p>
         <p className="text-lg">{item.title}</p>
