86c78565efa0e2f6329a3e4cf081e8d9525e499d
Jii-Yeong <tj5134@naver.com>
2024-11-27 18:36:41 +0900

    feat: [@yeong/utils] 파일 풀네임을 받으면 이름과 확장자를 분리해주는 util 함수 작성

--- packages/utils/src/string/string.utils.ts
+++ packages/utils/src/string/string.utils.ts
@@ -1,4 +1,14 @@
 export const parseDomSizeValue = (value: string | number, unit = 'px') => {
-  if (typeof value === 'string') return value
-  return `${value}${unit}`
-}
+  if (typeof value === 'string') return value;
+  return `${value}${unit}`;
+};
+
+export const getFileInfo = (fullName: string) => {
+  const point = fullName.lastIndexOf('.');
+  const name = fullName.substring(0, point);
+  const extension = fullName.substring(point + 1, fullName.length);
+  return {
+    name,
+    extension,
+  };
+};
