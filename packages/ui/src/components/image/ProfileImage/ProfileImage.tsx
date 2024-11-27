04428c659bd6d3db798ab6c5c2a86cb08ac73960
Jii-Yeong <tj5134@naver.com>
2024-11-27 16:23:26 +0900

    fix: [@yeong/ui] ProfileImage 컴포넌트 기본 이미지 추가 및 cva 사용

--- packages/ui/src/components/image/ProfileImage/ProfileImage.tsx
+++ packages/ui/src/components/image/ProfileImage/ProfileImage.tsx
@@ -1,25 +1,70 @@
-import { parseDomSizeValue } from '@yeong/utils/string';
+import { cva, VariantProps } from 'class-variance-authority';
+import { forwardRef, memo, useMemo } from 'react';
+import { ClassNameValue } from 'tailwind-merge';
+import { cn } from '../../../utils/class-name.utils.ts';
+
+const profileImageWrapperVariants = cva(
+  [
+    'relative',
+    'rounded-full',
+    'overflow-hidden',
+    'border',
+    'border-gray',
+    'bg-white',
+    'flex',
+    'flex-row',
+    'justify-center',
+    'items-center',
+  ],
+  {
+    variants: {
+      size: {
+        small: ['w-[32px]', 'h-[32px]', 'min-w-[32px]', 'min-h-[32px]'],
+        medium: ['w-[50px]', 'h-[50px]', 'min-w-[50px]', 'min-h-[50px]'],
+        large: ['w-[80px]', 'h-[80px]', 'min-w-[80px]', 'min-h-[80px]'],
+        xLarge: ['w-[120px]', 'h-[120px]', 'min-w-[120px]', 'min-h-[120px]'],
+      },
+    },
+    defaultVariants: {
+      size: 'small',
+    },
+  },
+);
+
+type ProfileImageWrapperVariant = VariantProps<
+  typeof profileImageWrapperVariants
+>;
 
 type ProfileImageProps = {
   imageSrc?: string;
-  size?: string | number;
+  size?: ProfileImageWrapperVariant['size'];
+  className?: ClassNameValue;
 };
 
-export default function ProfileImage({
-  imageSrc,
-  size = 30,
-}: ProfileImageProps) {
-  return (
-    <div
-      className="relative w-10 rounded-full overflow-hidden border border-gray bg-white"
-      style={{
-        width: parseDomSizeValue(size),
-        height: parseDomSizeValue(size),
-      }}
-    >
-      {imageSrc && (
-        <img src={imageSrc} alt="profile-image" className="w-full" />
-      )}
-    </div>
-  );
-}
+const ProfileImage = forwardRef(
+  ({ imageSrc, size, className }: ProfileImageProps) => {
+    const defaultImage =
+      'https://4rwpwj6q9lf5hlkz.public.blob.vercel-storage.com/common/images/default-profile-image-66IPBxnoDBpj8MMfqGw5hh6BM00rm6.png';
+
+    const divClassName = useMemo(
+      () => cn(profileImageWrapperVariants({ size }), className),
+      [size, className],
+    );
+    return (
+      <div className={divClassName}>
+        <img
+          src={imageSrc || defaultImage}
+          alt="profile-image"
+          className="w-full"
+        />
+      </div>
+    );
+  },
+);
+
+ProfileImage.displayName = 'ProfileImage';
+
+const MemoizedProfileImage = memo(ProfileImage);
+
+export { MemoizedProfileImage as default, profileImageWrapperVariants };
+export type { ProfileImageProps, ProfileImageWrapperVariant };
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,49 +1,64 @@
-"use client"
+'use client';
 
-import CommonButton, { CommonButtonProps } from './components/button/CommonButton/CommonButton.tsx';
-import ToggleButton, { ToggleButtonProps } from './components/button/ToggleButton/ToggleButton.tsx';
-import CommonChip from './components/chip/CommonChip/CommonChip.tsx';
-import { CommonChipProps } from './components/chip/CommonChip/CommonChip.tsx';
+import CommonButton, {
+  CommonButtonProps,
+} from './components/button/CommonButton/CommonButton.tsx';
+import ToggleButton, {
+  ToggleButtonProps,
+} from './components/button/ToggleButton/ToggleButton.tsx';
+import CommonChip, {
+  CommonChipProps,
+} from './components/chip/CommonChip/CommonChip.tsx';
 import CommonDivider from './components/divider/CommonDivider.tsx';
-import CommonDropdown, { CommonDropdownProps } from './components/dropdown/CommonDropdown/CommonDropdown.tsx';
-import ProfileImage from './components/image/ProfileImage/ProfileImage.tsx';
+import CommonDropdown, {
+  CommonDropdownProps,
+} from './components/dropdown/CommonDropdown/CommonDropdown.tsx';
+import CommonDropdownInner from './components/dropdown/CommonDropdownInner/CommonDropdownInner.tsx';
+import CommonDropdownItem, {
+  CommonDropdownItemProps,
+} from './components/dropdown/CommonDropdownItem/CommonDropdownItem.tsx';
+import ProfileImage, {
+  ProfileImageProps,
+  ProfileImageWrapperVariant,
+  profileImageWrapperVariants,
+} from './components/image/ProfileImage/ProfileImage.tsx';
+import CommonFileInput from './components/input/CommonFileInput/CommonFileInput.tsx';
 import CommonInput from './components/input/CommonInput/CommonInput.tsx';
 import CommonTextarea from './components/input/CommonTextarea/CommonTextarea.tsx';
 import LoadingSpinner from './components/loading/LoadingSpinner/LoadingSpinner.tsx';
 import CommonSkeleton from './components/skeleton/CommonSkeleton.tsx';
 import EllipsisText from './components/text/EllipsisText/EllipsisText.tsx';
-import CommonDropdownInner from './components/dropdown/CommonDropdownInner/CommonDropdownInner.tsx';
-import CommonDropdownItem from './components/dropdown/CommonDropdownItem/CommonDropdownItem.tsx';
-import CommonFileInput from './components/input/CommonFileInput/CommonFileInput.tsx';
 import CommonToast from './components/toast/CommonToast/CommonToast.tsx';
 import CommonToaster from './components/toast/CommonToaster/CommonToaster.tsx';
 import { useToast } from './hooks/useToast.tsx';
-import { CommonDropdownItemProps } from './components/dropdown/CommonDropdownItem/CommonDropdownItem.tsx';
 
 export {
   CommonButton,
-  ToggleButton,
   CommonChip,
   CommonDivider,
   CommonDropdown,
-  CommonInput,
-  CommonSkeleton,
-  CommonTextarea,
-  EllipsisText,
-  LoadingSpinner,
-  ProfileImage,
   CommonDropdownInner,
   CommonDropdownItem,
   CommonFileInput,
+  CommonInput,
+  CommonSkeleton,
+  CommonTextarea,
   CommonToast,
   CommonToaster,
-  useToast
+  EllipsisText,
+  LoadingSpinner,
+  ProfileImage,
+  profileImageWrapperVariants,
+  ToggleButton,
+  useToast,
 };
 
 export type {
-  CommonChipProps,
-  CommonDropdownProps,
-  CommonDropdownItemProps,
   CommonButtonProps,
+  CommonChipProps,
+  CommonDropdownItemProps,
+  CommonDropdownProps,
+  ProfileImageProps,
+  ProfileImageWrapperVariant,
   ToggleButtonProps,
-}
+};
