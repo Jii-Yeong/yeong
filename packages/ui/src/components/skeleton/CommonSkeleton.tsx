ef068f95562817b5ee958587319a8d5bcaff2c6c
Jii-Yeong <tj5134@naver.com>
2024-11-27 20:00:18 +0900

    fix: [@yeong/ui] CommonSkeleton 컴포넌트 cva 적용 및 props 조정

--- packages/ui/src/components/skeleton/CommonSkeleton.tsx
+++ packages/ui/src/components/skeleton/CommonSkeleton.tsx
@@ -1,22 +1,41 @@
+import { cva, VariantProps } from 'class-variance-authority';
 import { useMemo } from 'react';
 import { ClassNameValue } from 'tailwind-merge';
 import { cn } from '../../utils/class-name.utils.ts';
+
+const skeletonVariants = cva(
+  ['w-full', 'h-full', 'bg-light-gray', 'animate-pulse'],
+  {
+    variants: {
+      shape: {
+        rectangle: 'rounded-[16px]',
+        circle: 'rounded-full',
+      },
+    },
+    defaultVariants: {
+      shape: 'rectangle',
+    },
+  },
+);
+
+type SkeletonVariants = VariantProps<typeof skeletonVariants>;
 
 type CommonSkeletonProps = {
   width?: string | number;
   height?: string | number;
   className?: ClassNameValue;
+  shape?: NonNullable<SkeletonVariants['shape']>;
 };
 
 export default function CommonSkeleton({
   width,
   height,
   className,
+  shape,
 }: CommonSkeletonProps) {
   const divClassName = useMemo(
-    () =>
-      cn(['w-full', 'h-full', 'rounded-[16px]', 'bg-light-gray'], className),
-    [className],
+    () => cn(skeletonVariants({ shape }), className),
+    [shape, className],
   );
   return (
     <div
