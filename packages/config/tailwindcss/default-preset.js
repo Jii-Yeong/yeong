80fce5907b8c243d284f21b5d15e5c70b2745d14
Jii-Yeong <tj5134@naver.com>
2024-11-29 15:33:37 +0900

    fix: [@yeong/config] tailwind css preset 수정

--- packages/config/tailwindcss/default-preset.js
+++ packages/config/tailwindcss/default-preset.js
@@ -3,7 +3,7 @@ export default {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    '../../packages/@yeong/ui/**/*.{js,jsx,ts,tsx}',
+    './node_modules/@yeong/ui/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {
