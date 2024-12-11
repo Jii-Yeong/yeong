eb8253464351cef25c64582fa9b39e650358b75b
Jii-Yeong <tj5134@naver.com>
2024-12-11 16:02:15 +0900

    bugfix: [daily-routine] 카테고리 클릭 시 카테고리 이름이 바뀌지 않는 현상 수정

--- apps/daily-routine/src/components/todo-page/TodoDashboard/TodoDashboard.tsx
+++ apps/daily-routine/src/components/todo-page/TodoDashboard/TodoDashboard.tsx
@@ -2,18 +2,11 @@ import DepsTodoList from '@/components/todo-item/DepsTodoList/DepsTodoList';
 import './TodoDashboard.scoped.scss';
 import { useMemo } from 'react';
 import { useTodoCategory } from '@/hooks/todo/useTodoCategory';
-import { useSearchParams } from 'react-router-dom';
 
 export default function TodoDashboard() {
-  const [searchParams] = useSearchParams();
-  const { categoryList } = useTodoCategory();
-  const categoryName = useMemo(
-    () =>
-      categoryList.find(
-        (item) => item.id === Number(searchParams.get('category_id'))
-      )?.name || '전체',
-    [categoryList, searchParams]
-  );
+  const { category } = useTodoCategory();
+  const categoryName = useMemo(() => category?.name || '전체', [category]);
+
   return (
     <div className="todo-dash-board">
       <p>{categoryName}</p>
