4ba8301915ce269487c4ef4e7c4aefe8c8f7db11
Jii-Yeong <tj5134@naver.com>
2024-12-18 16:11:10 +0900

    chore: [readly] 콘솔 로그 삭제

--- apps/readly/src/service/book.service.ts
+++ apps/readly/src/service/book.service.ts
@@ -81,7 +81,6 @@ export const getBookSummaryListQuery = (params: BookSummaryListRequest) => {
       params.order,
     ],
     queryFn: async ({ pageParam = 0 }) => {
-      console.log(pageParam, params.limit);
       const { total, list, nextOffset } = await getBookSummaryList({
         ...params,
         offset: pageParam * Number(params.limit || 16),
