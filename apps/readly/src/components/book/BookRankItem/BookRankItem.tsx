ea40d034499666ac15a197a72e1f24b0f929d95a
Jii-Yeong <tj5134@naver.com>
2024-12-22 11:10:53 +0900

    fix: [readly] 등록한 책 랭킹에 숫자 표기 제거

--- apps/readly/src/components/book/BookRankItem/BookRankItem.tsx
+++ apps/readly/src/components/book/BookRankItem/BookRankItem.tsx
@@ -1,55 +1,23 @@
 import { BookCreatedRankModel } from '@/model/book/book.model';
 import Image from 'next/image';
-import { useMemo } from 'react';
-import { cva } from 'class-variance-authority';
 import Link from 'next/link';
 import { EllipsisText } from '@yeong/ui';
 
 export type BookRankItemProps = BookCreatedRankModel;
-
-const rankTextVariants = cva(['text-lg', 'font-bold', 'w-[50px]'], {
-  variants: {
-    rank: {
-      first: ['text-[#f5d706]'],
-      second: ['text-[#9b9b9b]'],
-      third: ['text-[#c06e01]'],
-    },
-  },
-});
 
 export default function BookRankItem({
   author,
   count,
   image,
   link,
-  rank,
   title,
 }: BookRankItemProps) {
-  const rankLabel = useMemo(() => {
-    if (rank === 1) return 'first';
-    else if (rank === 2) return 'second';
-
-    return 'third';
-  }, [rank]);
-
-  const rankTitle = useMemo(() => {
-    switch (rankLabel) {
-      case 'first':
-        return '1ST';
-      case 'second':
-        return '2ND';
-      case 'third':
-        return '3RD';
-    }
-  }, [rankLabel]);
-
   return (
     <Link
       href={link}
       target="_blank"
       className="flex flex-row items-center gap-x-[8px] w-full"
     >
-      <p className={rankTextVariants({ rank: rankLabel })}>{rankTitle}</p>
       <div className="md:hover:bg-light-gray rounded-[8px] p-[16px] flex flex-row items-center gap-x-[16px] w-full transition-colors">
         <Image
           src={image}
--- apps/readly/src/components/skeleton/book/BookRankItemSkeleton.tsx
+++ apps/readly/src/components/skeleton/book/BookRankItemSkeleton.tsx
@@ -3,7 +3,6 @@ import { CommonSkeleton } from '@yeong/ui';
 export default function BookRankItemSkeleton() {
   return (
     <div className="flex flex-row items-center gap-x-[8px] w-full">
-      <CommonSkeleton width={50} height={30} className="rounded-[8px]" />
       <div className="rounded-[8px] p-[16px] flex flex-row items-center gap-x-[16px] w-full">
         <CommonSkeleton width={100} height={145} />
         <div className="flex flex-col gap-y-[16px] flex-1">
