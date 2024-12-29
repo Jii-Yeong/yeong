e540dbdff679c874b4a19a84dcbaa1730e9902bb
Jii-Yeong <tj5134@naver.com>
2024-12-29 15:02:54 +0900

    fix: [readly] 요약 검색 시 카테고리 선택 후 카테고리 리스트 드롭다운 띄우도록 수정

--- apps/readly/src/components/book/SearchBookSummary/SearchBookSummary.tsx
+++ apps/readly/src/components/book/SearchBookSummary/SearchBookSummary.tsx
@@ -1,5 +1,10 @@
+import { useClickOpenOutside } from '@/hooks/useClickOpenOutside';
+import { BookCategoryDto } from '@/model/book/book.dto';
+import { getBookCategoryListQuery } from '@/service/book.service';
+import { Icon } from '@iconify/react/dist/iconify.js';
 import {
   CommonButton,
+  CommonChip,
   CommonDropdown,
   CommonDropdownInner,
   CommonDropdownItem,
@@ -7,7 +12,7 @@ import {
   CommonInput,
 } from '@yeong/ui';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export const searchTypeList: CommonDropdownItemProps[] = [
   {
@@ -31,9 +36,27 @@ export default function SearchBookSummary() {
   const [option, setOption] = useState('title');
   const [value, setValue] = useState('');
   const [alertText, setAlertText] = useState('');
+  const [selectedCategoryList, setSelectedCategoryList] = useState<string[]>(
+    [],
+  );
+  const searchResultRef = useRef<HTMLDivElement | null>(null);
+  const { isOpen: isOpenSearchResult, setIsOpen: setIsOpenSearchResult } =
+    useClickOpenOutside({
+      ref: searchResultRef,
+    });
+
+  const { data: categoryData } = getBookCategoryListQuery();
+  const [categoryList, setCategoryList] = useState<BookCategoryDto[]>([]);
 
   const changeValue = (value: string) => {
     setValue(value);
+
+    if (option === 'category' && categoryData) {
+      const filteredList = categoryData.filter((item) => {
+        return item.name.includes(value);
+      });
+      setCategoryList(filteredList);
+    }
   };
 
   const changeOptionValue = (value: string) => {
@@ -41,16 +64,42 @@ export default function SearchBookSummary() {
   };
 
   const clickSearchButton = () => {
-    if (!value) {
+    if (!value && selectedCategoryList.length <= 0) {
       setAlertText('검색어를 입력해주세요.');
       return;
     }
     currentParams.set('type', option);
-    currentParams.set('keyword', value);
+    currentParams.set(
+      'keyword',
+      option === 'category' ? JSON.stringify(selectedCategoryList) : value,
+    );
     router.push(
       `${process.env.NEXT_PUBLIC_CLIENT_URL}/summary/search?${currentParams.toString()}`,
     );
   };
+
+  const clickCategoryItem = (name: string) => {
+    const addedList = [...selectedCategoryList];
+    if (addedList.includes(name)) return;
+    addedList.push(name);
+    setSelectedCategoryList(addedList);
+    setValue('');
+    setCategoryList(categoryData || []);
+  };
+
+  const clickRemoveCategoryItem = (name: string) => {
+    const filteredList = selectedCategoryList.filter((item) => item !== name);
+    setSelectedCategoryList(filteredList);
+  };
+
+  const focusInputArea = () => {
+    if (option === 'category') setIsOpenSearchResult(true);
+  };
+
+  useEffect(() => {
+    if (!categoryData) return;
+    setCategoryList(categoryData);
+  }, [categoryData]);
   return (
     <div className="flex flex-row gap-x-[16px] w-full">
       <CommonDropdown
@@ -66,14 +115,49 @@ export default function SearchBookSummary() {
           ))}
         </CommonDropdownInner>
       </CommonDropdown>
-      <CommonInput
-        className="flex-1"
-        value={value}
-        onChangeValue={changeValue}
-        placeholder="검색어를 입력하세요."
-        onEnter={clickSearchButton}
-        alertText={alertText}
-      />
+      <div className="w-full relative flex-1">
+        <CommonInput
+          value={value}
+          onChangeValue={changeValue}
+          placeholder="검색어를 입력하세요."
+          onEnter={clickSearchButton}
+          alertText={alertText}
+          innerClassName="min-w-[150px]"
+          onFocus={focusInputArea}
+        >
+          {selectedCategoryList.map((item) => (
+            <CommonChip
+              text={item}
+              key={item}
+              variant="secondary"
+              isActive
+              removeIcon={<Icon icon="material-symbols:close-rounded" />}
+              onClickRemove={() => clickRemoveCategoryItem(item)}
+            />
+          ))}
+        </CommonInput>
+        {isOpenSearchResult && (
+          <div
+            className="absolute w-full bg-white shadow-lg shadow-gray/80 rounded-[8px] overflow-hidden z-20"
+            ref={searchResultRef}
+          >
+            <div className="flex flex-wrap p-[12px]">
+              {categoryList.length > 0 &&
+                categoryList.map((item) => (
+                  <div className="w-[130px] p-[8px] " key={item.id}>
+                    <CommonChip
+                      text={item.name}
+                      variant="secondary"
+                      isActive
+                      onClick={() => clickCategoryItem(item.name)}
+                    />
+                    {/* {item.name} */}
+                  </div>
+                ))}
+            </div>
+          </div>
+        )}
+      </div>
       <CommonButton onClick={clickSearchButton}>검색</CommonButton>
     </div>
   );
